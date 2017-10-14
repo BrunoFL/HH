937eed9e1dcfaa9cac922b867570f943a51e3b3f
bfollet <follet-bruno@hotmail.fr>
2017-10-14 16:48:19 +0200

    Ajout slider composants accordéon

--- js/pageMange.js
+++ js/pageMange.js
@@ -19,7 +19,7 @@ function pageMange() {
     $('list-aliment').scrollspy({
         target : '#list-aliment-content'
     });  // FONCTIONNE PAS !!!
-    // $('.collapse').collapse();
+    $('.collapse').on('show.bs.collapse', adjustAliment);
 };
 
 function create_list_header() {
@@ -58,12 +58,37 @@ function create_list_element() {
                 collapse + '" aria-expanded="false" aria-controls="' +
                 collapse + '">' + name + '</a></h5></div>';
 
-            list +=
-                '<div id="' + collapse +
-                '" class="collapse" role="tabpanel" aria-labelledby="' +
-                heading + '" data-parent="#' + accordion +
-                '"><div class="card-body">kjrgkjlerkjlgnkn</div></div></div></div>';
+            list += '<div id="' + collapse +
+                    '" class="collapse" role="tabpanel" aria-labelledby="' +
+                    heading + '" data-parent="#' + accordion +
+                    '"><div class="card-body">' + Math.random() +
+                    '</div></div></div></div>';
         }
         }
     return list + '</div>';
+    }
+
+function adjustAliment(e) {
+    $('.collapse').collapse('hide');
+    var el = e.target;
+
+    var html =
+        '<div class="card-body"><div class="row"><h6>DETAIL</h6><ul class="list-inline"><li class="list-inline-item">P</li><li class="list-inline-item">P</li></ul></nav></div><div class="row"><form class="form-inline"><div class="col-7">';
+    html +=
+        '<input id="ex1" type="text" readonly class="form-control-plaintext" class="col-8">';
+    html +=
+        '</div><div class="col-5 input-group"><input type="number" class="form-control" id="inputPassword2" placeholder="10"><span class="input-group-addon">g</span></div>';
+    html +=
+        '<ul class="list-inline"><li class="list-inline-item"><span id="aliment_glucide">9</span> Glucides</li><li class="list-inline-item"><span id="aliment_lipide">5</span> Lipides</li><li class="list-inline-item"><span id="aliment_proteine">9</span> Proteines</li><li class="list-inline-item"><span id="aliment_kcal">120</span> Kcal</li></ul></form></div></div>';
+
+    el.innerHTML = html;
+    $('#ex1').slider({
+        formatter : function(value) { return 'Current value: ' + value; },
+        width : '50%'
+    });
+    /*
+    "libelle": "Pomme", "detail": "Une pomme du coup", "mesureMax": 0.0,
+        "mesurePas": 0.0, "typeMesurePas": 0.0, "quantitePortionRef": 0.0,
+        "typeMesurePortionRef": 0.0, "qteGlucide": 0.0, "qteProteine": 0.0,
+        "qteLipide": 0.0, "qteCalorie": 0.0*/
 }
